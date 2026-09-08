dd2dbe44cd1c2bd76bc0cdb676f526f47769cdb9
anas-lowlevel <run.anasdev@gmail.com>
2026-09-09 01:18:37 +0300

    fix : Resolved compiler warnings and pedantic errors

---
+++ build-asan/CMakeFiles/bob16.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bob16.dir/link.d"
+  "CMakeFiles/bob16.dir/src/main.c.o"
+  "CMakeFiles/bob16.dir/src/main.c.o.d"
+  "bob16"
+  "bob16.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bob16.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
