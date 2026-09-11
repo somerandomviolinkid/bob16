fcf9d8610fff38be4c9108e6c5fe2df84b5fe116
surrealism <77070074+surrealism21@users.noreply.github.com>
2026-09-11 17:25:43 -0400

    c99 strict compatiblity
    this is a nothing change lol

--- main.c
+++ main.c
@@ -344,7 +344,7 @@ void execute() {
 					
 					cpu.pc = cpu.regFile[7];
 					break;
-				case GETS:
+				case GETS: {
 					int n = cpu.regFile[1];
 					cpu.regFile[7] = cpu.pc;
 					char buf[n];
@@ -355,6 +355,7 @@ void execute() {
 
 					cpu.pc = cpu.regFile[7];
 					break;
+				}
 			}
 
 			break;
